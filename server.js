7d280bc96becc6fa26f8e7db7d12a0036653a627
amaykorade <amaykorade5@gmail.com>
2024-11-03 17:40:08 +0530

    changed cors policy

--- server.js
+++ server.js
@@ -36,11 +36,11 @@ server.use((req, res, next) => {
     const origin = req.headers.origin;
     if (allowedOrigins.includes(origin)) {
         res.header('Access-Control-Allow-Origin', origin);
+        res.header('Access-Control-Allow-Credentials', 'true');
+        res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+        res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     }
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-    // res.header('Access-Control-Allow-Methods', '*');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    // return ok for preflight request.
+
     if (req.method == 'OPTIONS') {
         return res.sendStatus(200);
     }
